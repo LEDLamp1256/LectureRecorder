05bd05110013097340a8614fc6672e04ae4fc3b3
Dylan Lee <dylanlee0613@gmail.com>
2026-08-30 23:06:36 +0800

    Initial Commit

---
+++ LectureRecorder/LectureRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LectureRecorderApp.swift
+//  LectureRecorder
+//
+//  Created by Dylan Lee on 8/30/26.
+//
+
+import SwiftUI
+
+@main
+struct LectureRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
